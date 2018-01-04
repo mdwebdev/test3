db751c0ccd11aed127993abc850a50c28943fcdc
mihail <test@test.ru>
2018-01-04 16:58:54 +0300

    add project

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+
+
+	<?php
+
+		setcookie("login", "fgsdgsdg", time() + 3600 * 24 * 7);
+
+		echo $_COOKIE['login'];
+
+	?>
+	
+</body>
+</html>
